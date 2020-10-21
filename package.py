7b0d394fd28d0b9626ba9a88fa1cf248e96c8aa5
Benjamin Skinner <benjamin.skinner96@gmail.com>
2020-10-21 15:57:16 -0400

    Initial wipe clean for houdini deps and package for oiio-2.0.10-houdini

---
+++ package.py
@@ -0,0 +1,35 @@
+name = 'oiio'
+
+version = '2.0.10-houdini-ta.1.0.0'
+
+authors = [
+    'benjamin.skinner',
+]
+
+requires = [
+    'libtiff',
+    'libpng',
+]
+
+variants = [
+    ['platform-windows', 'arch-x64', 'os-windows-10', 'houdini-18.0.532'],
+]
+
+private_build_requires = [
+    'python-3'
+]
+
+build_command = "python {root}/rez_build.py"
+
+def commands():
+
+    # Split and store version and package version
+    split_versions = str(version).split('-')
+    env.OIIO_VERSION.set(split_versions[0])
+    env.OIIO_PACKAGE_VERSION.set(split_versions[1])
+
+    env.OPENIMAGEIO_ROOT_DIR.set( "{root}" )
+    
+    env.OIIO_LIB_DIR.set( "{root}/lib" )
+    env.OIIO_INCLUDE_DIR.set( "{root}/include" )
+    env.PATH.append( "{root}/bin" )
---
+++ rez_build.py
@@ -0,0 +1,31 @@
+import os, subprocess, sys
+import shutil
+
+# TODO: We should build glew properly, but ben skinner didn't have the time
+# I'm terribly sorry...
+
+if __name__ == "__main__":
+    src = os.environ["HOUDINI_ROOT"]
+    dst = os.environ["REZ_BUILD_INSTALL_PATH"]
+    inc_dst = os.environ["REZ_BUILD_INSTALL_PATH"] + "/include"
+    lib_dst = os.environ["REZ_BUILD_INSTALL_PATH"] + "/lib"
+
+    
+    if 'win' in str(sys.platform):
+
+        # Remove existing build
+        if os.path.exists(dst):
+            print(" - Removing existing build")
+            shutil.rmtree(dst, ignore_errors=True)
+
+
+        print ("Starting install")
+
+        shutil.copytree(src + "/toolkit/include/OpenImageIO", inc_dst + "/OpenImageIO")
+        
+        os.mkdir(lib_dst)
+
+        shutil.copy(src + '/custom/houdini/dsolib/OpenImageIO_sidefx.lib', lib_dst + "/OpenImageIO.lib")
+        shutil.copy(src + '/custom/houdini/dsolib/OpenImageIO_Util_sidefx.lib', lib_dst + "/OpenImageIO_Util.lib")
+
+        print ("Completed install")
