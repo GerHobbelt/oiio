924eb902e40c9c12566798564454396da3f86e25
nico.vandenbosch <nico.vandenbosch@mythvfx.com>
2021-06-28 22:29:39 -0700

    stripping LA mount from install prefix

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "oiio"
 
-version = "1.5.24.x.1.0.1"
+version = "1.5.24.x.1.0.2"
 
 authors = [
     "Larry Gritz and Co."
@@ -23,9 +23,14 @@ variants = [
 ]
 
 # this is a pretty ugly way of building all the different variants but it works
+# also, for some reason rez expands ths install_path to include the '/mnt/oracle/la', so
+# we include some extra nastiness to strip it off so it doesn't get put into the RPATH of
+# libraries
 build_command = "cd {root};make clean;" \
-                "[ {variant_index}==0 ] && make INSTALLDIR={install_path} USE_NUKE=1 NUKE_HOME=/usr/local/Nuke11.3v6 NUKE_VERSION=11.3 " \
-                " || make INSTALLDIR={install_path} USE_NUKE=1 NUKE_HOME=/usr/local/Nuke12.2v3 NUKE_VERSION=12.2"
+                "export install_path=`echo {install_path} | sed 's/\/mnt\/oracle\/la//g'`;" \
+                "[ {variant_index}==0 ] && make INSTALLDIR=$install_path USE_NUKE=1 NUKE_HOME=/usr/local/Nuke11.3v6 NUKE_VERSION=11.3 " \
+                " || make INSTALLDIR=$install_path USE_NUKE=1 NUKE_HOME=/usr/local/Nuke12.2v3 NUKE_VERSION=12.2"
+
 
 uuid = "oiio"
 
