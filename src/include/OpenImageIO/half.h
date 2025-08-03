af0b15be20ed58d5e7ab9b9c60c53082d7def892
Ger Hobbelt <ger@hobbelt.com>
2025-08-03 22:37:07 +0200

    fix compiler errors: undefined `fmt::ostream_formatter`

--- src/include/OpenImageIO/half.h
+++ src/include/OpenImageIO/half.h
@@ -18,6 +18,9 @@
 #if defined(FMT_VERSION) && !defined(OIIO_HALF_FORMATTER)
 #    if FMT_VERSION >= 100000
 #        define OIIO_HALF_FORMATTER
+
+#        include <fmt/ostream.h>
+
 FMT_BEGIN_NAMESPACE
 template<> struct formatter<half> : ostream_formatter {};
 FMT_END_NAMESPACE
